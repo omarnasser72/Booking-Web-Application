2cc74447d00e19d6e7e2d72fba8cd71d734fc840
omarnasser72 <omarnasser7272@gmail.com>
2023-10-15 00:39:30 +0300

    editing .js in home

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 import { SearchContext } from "../../context/SearchContext";
-import api from "./api.js";
+import api from "../../api.js";
 
 const Home = () => {
   const { date: contextDate } = useContext(SearchContext);
