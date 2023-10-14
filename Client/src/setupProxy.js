0a04770b53bb14230278723224ea19cf517995f4
omarnasser72 <omarnasser7272@gmail.com>
2023-10-14 21:52:38 +0300

    adding setupProxy.js

---
+++ Client/src/setupProxy.js
@@ -0,0 +1,11 @@
+import createProxyMiddleware from "http-proxy-middleware";
+
+module.exports = function (app) {
+  app.use(
+    "/",
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_URL,
+      changeOrigin: true,
+    })
+  );
+};
