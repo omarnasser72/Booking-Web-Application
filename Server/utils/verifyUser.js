7fd9178cdec62a7c3778d1d05bef3024c548c051
omarnasser72 <omarnasser7272@gmail.com>
2023-10-16 22:25:20 +0300

    try token based

--- Server/utils/verifyUser.js
+++ Server/utils/verifyUser.js
@@ -1,48 +1,49 @@
 import jwt from "jsonwebtoken";
 import { createError } from "../utils/error.js";
 
-export const verifyToken = (req, res, next) => {
-  if (!req.cookies.accessToken)
-    return next(createError(401, "You aren't authenticated (no token)"));
-
-  const token = req.cookies.accessToken;
-  jwt.verify(token, process.env.JWT, (err, decoded) => {
-    if (err) {
-      return next(createError(403, "token isn't valid"));
-    }
-    req.user = decoded;
-
-    next();
-  });
-};
-
 // export const verifyToken = (req, res, next) => {
-//   const authHeader = req.headers["authorization"];
-
-//   if (!authHeader) {
+//   if (!req.cookies.accessToken)
 //     return next(createError(401, "You aren't authenticated (no token)"));
-//   }
 
-//   const token = authHeader.replace("Bearer ", ""); // Assuming the token is sent as "Bearer your-token-here"
-
+//   const token = req.cookies.accessToken;
 //   jwt.verify(token, process.env.JWT, (err, decoded) => {
-//     console.log("Decoded token:", decoded);
 //     if (err) {
-//       console.error("Token verification error:", err);
-//       return next(createError(403, "Token isn't valid"));
+//       return next(createError(403, "token isn't valid"));
 //     }
 //     req.user = decoded;
-//     console.log("req.user", req.user);
+
 //     next();
 //   });
 // };
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return next(createError(401, "You aren't authenticated (no token)"));
+  }
+
+  const tokenData = authHeader.split(" ");
+
+  if (tokenData[0] !== "Bearer" || !tokenData[1]) {
+    return next(createError(403, "Invalid token format"));
+  }
+
+  jwt.verify(tokenData[1], process.env.JWT, (err, decoded) => {
+    if (err) {
+      return next(createError(403, "Token isn't valid"));
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 export const verifyUser = (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user) {
       next();
     } else {
-      return next(createError(405, "You aren't authorized"));
+      return next(createError(403, "You aren't user"));
     }
   });
 };
@@ -52,7 +53,7 @@ export const verifyAdmin = (req, res, next) => {
     if (req.user && req.user.isAdmin) {
       next();
     } else {
-      return next(createError(403, "You aren't authorized"));
+      return next(createError(403, "You aren't admin"));
     }
   });
 };
