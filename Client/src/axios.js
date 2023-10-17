7d1838fa05e9c335801797b4464ca29470392fec
omarnasser72 <omarnasser7272@gmail.com>
2023-10-18 01:03:06 +0300

    try to add header in custom axios

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
+  headers: {
+    Authorization: localStorage.getItem("accessToken"),
+  },
   baseURL: "https://booking-fwaz.onrender.com",
 });
