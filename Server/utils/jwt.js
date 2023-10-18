1cf26bb62bb168cbc0387173286cf9708f970197
omarnasser72 <omarnasser7272@gmail.com>
2023-10-18 13:40:20 +0300

    remove validateToken from verifyUser & verifyAdmin

--- Server/utils/jwt.js
+++ Server/utils/jwt.js
@@ -10,47 +10,78 @@ export const createToken = (user) => {
 };
 
 export const validateToken = (req, res, next) => {
+  console.log("req.headers:", req.headers);
   const cookieHeader = req.headers.cookie;
-  const cookies = cookieHeader.split("; ");
-  let accessToken;
 
-  for (const cookie of cookies) {
-    const [name, value] = cookie.split("=");
-    if (name === "accessToken") {
-      accessToken = value;
-      break;
-    }
-  }
-
-  if (!accessToken) {
+  if (!cookieHeader) {
     next(createError(400, "no token exists"));
-  }
-  try {
-    const decoded = jwt.verify(accessToken, process.env.JWT);
-    req.user = decoded;
-    req.authenticated = true;
-    next();
-  } catch (error) {
-    next(error);
+  } else {
+    const cookies = cookieHeader.split("; ");
+    let accessToken;
+
+    for (const cookie of cookies) {
+      const [name, value] = cookie.split("=");
+      if (name === "accessToken") {
+        accessToken = value;
+        break;
+      }
+    }
+    console.log("req.headers:", req.headers);
+    if (!accessToken) accessToken = req.headers.authorization;
+    if (!accessToken) next(createError(400, "no token exists"));
+
+    try {
+      const decoded = jwt.verify(accessToken, process.env.JWT);
+      console.log(decoded);
+      req.user = decoded;
+      req.authenticated = true;
+      if (req.user) {
+        next();
+      } else {
+        return next(createError(403, "You aren't user"));
+      }
+    } catch (error) {
+      next(error);
+    }
   }
 };
 
 export const verifyUser = (req, res, next) => {
-  validateToken(req, res, () => {
+  console.log("req.headers:", req.headers);
+  const accessToken = req.headers.authorization;
+  if (!accessToken) next(createError(400, "no token exists"));
+
+  try {
+    const decoded = jwt.verify(accessToken, process.env.JWT);
+    console.log(decoded);
+    req.user = decoded;
+    req.authenticated = true;
     if (req.user) {
       next();
     } else {
       return next(createError(403, "You aren't user"));
     }
-  });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const verifyAdmin = (req, res, next) => {
-  validateToken(req, res, () => {
+  console.log("req.headers:", req.headers);
+  const accessToken = req.headers.authorization;
+  if (!accessToken) next(createError(400, "no token exists"));
+
+  try {
+    const decoded = jwt.verify(accessToken, process.env.JWT);
+    console.log(decoded);
+    req.user = decoded;
+    req.authenticated = true;
     if (req.user && req.user.isAdmin) {
       next();
     } else {
-      return next(createError(403, "You aren't admin"));
+      return next(createError(403, "You aren't Admin"));
     }
-  });
+  } catch (error) {
+    next(error);
+  }
 };
