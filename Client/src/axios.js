7239029adeecb0a8d14df7692885768098dd51e8
omarnasser72 <omarnasser7272@gmail.com>
2023-10-26 23:02:39 +0300

    add delay to axios

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -3,16 +3,24 @@ import orgaxios from "axios";
 const axios = orgaxios.create({ baseURL: "https://booking-fwaz.onrender.com" });
 //const axios = orgaxios.create({ baseURL: "http://localhost:8080" });
 
-// Get the token from localStorage
-const accessToken = localStorage.getItem("accessToken");
-console.log("accessToken:", accessToken);
-// If the token exists, set it as an Axios default header
-if (accessToken) {
-  axios.defaults.headers.common["Authorization"] = accessToken;
-  console.log(
-    `axios.defaults.headers.common["Authorization"]:`,
-    axios.defaults.headers.common["Authorization"]
-  );
-}
+// Function to wait for a specified duration in milliseconds
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Async function to wait for 1 second and then get the token from localStorage
+const getTokenWithDelay = async () => {
+  await delay(2000); // Wait for 1000 ms (1 second)
+  const accessToken = localStorage.getItem("accessToken");
+  console.log("accessToken:", accessToken);
+  // If the token exists, set it as an Axios default header
+  if (accessToken) {
+    axios.defaults.headers.common["Authorization"] = accessToken;
+    console.log(
+      `axios.defaults.headers.common["Authorization"]:`,
+      axios.defaults.headers.common["Authorization"]
+    );
+  }
+};
+
+getTokenWithDelay();
 
 export default axios;
