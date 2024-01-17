a8162a10537b4cc2d168a608b3007cdc8352da76
omarnasser72 <omarnasser7272@gmail.com>
2024-01-17 22:16:11 +0200

    adding reFetch after updating

--- Client/src/pages/singleUser/SingleUser.jsx
+++ Client/src/pages/singleUser/SingleUser.jsx
@@ -29,7 +29,7 @@ const COUNTRY_REGEX = /^[A-Za-z\s\']*([A-Za-z][A-Za-z\s\']*){3,}$/;
 const SingleUser = () => {
   const location = useLocation();
   const userId = location.pathname.split("/")[3];
-  const { data: user, loading, error } = useFetch(`/users/${userId}`);
+  const { data: user, loading, error, reFetch } = useFetch(`/users/${userId}`);
   const [sidebar, setSidebar] = useState(false);
 
   const [info, setInfo] = useState({});
@@ -202,6 +202,7 @@ const SingleUser = () => {
         const res = await axios.put(`/users/${user._id}`, newUser);
         console.log(res);
         setEditMode(false);
+        reFetch();
       } catch (error) {
         setErrMsg(error?.response?.data?.message);
         console.log(error);
