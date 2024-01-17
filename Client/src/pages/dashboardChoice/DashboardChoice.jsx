e0ea37b399d86a79d9464b3e9fb416f4429a0103
omarnasser72 <omarnasser7272@gmail.com>
2024-01-17 23:55:41 +0200

    return reload cond

--- Client/src/pages/dashboardChoice/DashboardChoice.jsx
+++ Client/src/pages/dashboardChoice/DashboardChoice.jsx
@@ -3,17 +3,17 @@ import "./dashboardChoice.css";
 import { useEffect } from "react";
 
 const Dashboard = () => {
-  // useEffect(() => {
-  //   // Check if the page needs to be reloaded
-  //   const needsReload = localStorage.getItem("needsReload");
-  //   if (needsReload) {
-  //     // Perform the reload
-  //     window.location.reload();
+  useEffect(() => {
+    // Check if the page needs to be reloaded
+    const needsReload = localStorage.getItem("needsReload");
+    if (needsReload) {
+      // Perform the reload
+      window.location.reload();
 
-  //     // Reset the flag to prevent further reloading
-  //     localStorage.removeItem("needsReload");
-  //   }
-  // }, []);
+      // Reset the flag to prevent further reloading
+      localStorage.removeItem("needsReload");
+    }
+  }, []);
   return (
     <div className="dashboardChoice">
       <button
